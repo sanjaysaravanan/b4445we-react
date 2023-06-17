e409b4c96eea805bcb163dfff9d3cc56ad4e42be
Sanjay Saravanan <sanjaysaravanan38@gmail.com>
2023-06-17 17:13:07 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 
+// default import      simple import
+import description, { name } from './sample.js';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {description}
+          Hello {name}. Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
---
+++ src/sample.js
@@ -0,0 +1,10 @@
+export const name = "B4445WETAMIL"; // simple export
+
+const description = "Sample Application to Learn React";
+
+export default description;
+
+
+// export {
+//   name
+// }
